c09525c342b8861b821078f6524745b195a04901
zahraaAhmed <zahraa.ahmed.ham@gmail.com>
2020-09-29 08:27:09 +0200

    pagination

--- services.php
+++ services.php
@@ -225,6 +225,22 @@
                        }
                        ?>
           </div>
+          <!-- start pag block -->
+          <div class="page-pagination">
+            <ul class="pagination">
+                    <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true"><i class="fas fa-angle-right"></i></a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                    <a class="page-link" href="#"><i class="fas fa-angle-left"></i></a>
+                    </li>
+            </ul>
+          </div>
       </div>
     </div>
 </section>
